f795517cb9c48c40b1b6ea9bd951a4e16b4b473e
reapzor <reapzor@gmail.com>
2016-01-06 21:26:07 -0500

    Use a collector intead of asserted array for bytesToHex method

--- src/main/java/com/bortbort/helpers/DataTypeHelpers.java
+++ src/main/java/com/bortbort/helpers/DataTypeHelpers.java
@@ -9,7 +9,7 @@ import java.nio.ByteBuffer;
 public class DataTypeHelpers {
 
     private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHexString(byte[] bytes) {
+    public static String bytesToHexString(byte... bytes) {
         char[] hexChars = new char[bytes.length * 2];
         for ( int j = 0; j < bytes.length; j++ ) {
             int v = bytes[j] & 0xFF;
